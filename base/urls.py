9a536d3f110516ded4cb50694869903262d6c516
M P <mayur112>
2024-01-06 17:42:20 +0530

    Register done

--- base/urls.py
+++ base/urls.py
@@ -5,12 +5,15 @@ from . import views
 
 urlpatterns=[
     path("",views.home,name="home"),
+
+
     path('room/<str:pk>/',views.room,name="room"),
     path('create-room',views.createroom,name="create-room"),
     path('update-room/<str:pk>/',views.updateroom,name="update-room"),
     path('delete-room/<str:pk>/',views.deleteroom,name="delete-room"),
 
     path('login/',views.login_page,name="login"),
+    path('register/',views.registerpage,name="register"),
     path('logout/',views.logoutuser,name="logout"),
 
 
--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -73,12 +74,14 @@ def deleteroom(request,pk):
 
 def login_page(request):
 
+    page='login'
+
 
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method =="POST":
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
 
         try:
@@ -94,9 +97,28 @@ def login_page(request):
         else:
             messages.error(request,"  username or password does not exist")
 
-    context={}
+    context={'page':page}
     return render(request,"base/login_register.html",context)
 
 def logoutuser(request):
     logout(request)
     return redirect('home')
+
+def registerpage(request):
+
+    page= 'register'
+    form= UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Error Occure During Registration")
+
+    context={'page':page,'form':form}
+    return render(request,'base/login_register.html',context)
